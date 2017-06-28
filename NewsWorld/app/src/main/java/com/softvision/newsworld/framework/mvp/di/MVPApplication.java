898ad4d644ad62fbe9221aa44b7f966f97df7057
RAC IT SOLUTIONS PVT.LTD <racitsolutionspvtltd@RACs-Mac-mini.local>
2017-06-28 14:34:02 +0530

    [Puneet]:News World Folder Structure added

---
+++ NewsWorld/app/src/main/java/com/softvision/newsworld/framework/mvp/di/MVPApplication.java
@@ -0,0 +1,48 @@
+package com.softvision.newsworld.framework.mvp.di;
+
+import android.app.Application;
+
+/**
+ * Created by racitsolutionspvtltd on 28/06/17.
+ */
+
+public class MVPApplication extends Application {
+    @Inject
+    DataManager mDataManager;
+
+    @Inject
+    CalligraphyConfig mCalligraphyConfig;
+
+    private ApplicationComponent mApplicationComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mApplicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this)).build();
+
+        mApplicationComponent.inject(this);
+
+        AppLogger.init();
+
+        AndroidNetworking.initialize(getApplicationContext());
+        if (BuildConfig.DEBUG) {
+            AndroidNetworking.enableLogging(Level.BODY);
+        }
+
+        CalligraphyConfig.initDefault(mCalligraphyConfig);
+    }
+
+    public ApplicationComponent getComponent() {
+        return mApplicationComponent;
+    }
+
+
+    // Needed to replace the component with a test specific one
+    public void setComponent(ApplicationComponent applicationComponent) {
+        mApplicationComponent = applicationComponent;
+    }
+}
+
+}
